1506b672319d9ef4ddff07ac24adc7f218454607
Bushra Fatima <bushrafatima@192.168.1.111>
2025-05-31 20:05:12 +0100

    added ap1-install file

---
+++ Section3/c2-ec2instance.tf
@@ -0,0 +1,25 @@
+/*
+block type - e.g. resource 
+(top) block label - e.g. "aws instance" "ec2demo"
+    - for resource block - you will have two block labels
+    - for variables block - you will have one block label 
+block inside block - provisioners, resource specific information like tags
+
+- in below example name is a local block label
+
+Resource Block Syntax
+resource type - determines the kind of infrastructure object 
+resource local name - how you refer to that aws resource so it can be referenced from elsewhere in the same terraform module
+meta-arguments - adds specification to said resource 
+resource-arguments - will be specific to resource type 
+
+*/
+
+resource "aws_instance" "myec2vm" {
+  ami           = "ami-0fc32db49bc3bfbb1"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "HelloWorld"
+  }
+}
